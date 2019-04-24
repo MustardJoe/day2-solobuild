aa50f9aee34fb4ee64a60a36fe7ce836831aa8bc
Jonathan Forney <jonathan.forney@gmail.com>
2019-04-23 17:25:10 -0700

    website finished, tests working, quiz working, final commit

---
+++ src/app.js
@@ -0,0 +1,35 @@
+function quizResult(question1, question2, question3, question4, question5) {
+    
+    question1 = question1.toLowerCase();
+    question2 = question2.toLowerCase();
+    question3 = question3.toLowerCase();
+    question4 = question4.toLowerCase();
+    question5 = question5.toLowerCase();
+    
+    let score = 0;   
+
+
+    if(question1 === 'morris' ) {
+        score += 1;
+    }
+
+    if(question2 === 'butter') {
+        score += 1;
+    }
+
+    if(question3 === 'black' ) {
+        score += 1;
+    }
+
+    if(question4 === 'coyote') {
+        score += 1;
+    }
+
+    if(question5 === 'crow') {
+        score += 1;
+    }
+
+   return score;
+
+}
+export default quizResult;
---
+++ src/index.js
@@ -0,0 +1,31 @@
+import quizResult from './app.js';
+
+const question1 = document.getElementById('q1');
+const question2 = document.getElementById('q2');
+const question3 = document.getElementById('q3');
+const question4 = document.getElementById('q4');
+const question5 = document.getElementById('q5');
+const submitButton = document.getElementById('quizButton');
+const quizMsg = document.getElementById('quiz-result');
+
+submitButton.addEventListener('click', () => {
+    const questionOneA = question1.value;
+    const questionTwoA = question2.value;
+    const questionThreeA = question3.value;
+    const questionFourA = question4.value;
+    const questionFiveA = question5.value;
+
+    const score = quizResult(questionOneA, questionTwoA, questionThreeA, questionFourA, questionFiveA);
+   
+
+  
+    const message = 'You got ' + score + '/5 correct';
+    
+    if (score === 5) {
+    quizMsg.textContent = message + ' You are a good cat knower!';
+    }
+    else {
+        quizMsg.textContent = message + ' but you missed at least one question.';
+    }
+
+});
