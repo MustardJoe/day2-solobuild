7355bde2e0da56563191c5d7211e816f2649fdc5
Jonathan Forney <jonathan.forney@gmail.com>
2019-04-24 17:39:34 -0700

    fixed previously missing file - hopfully

--- tests/index.js
+++ tests/index.js
@@ -1,23 +1,8 @@
-import getHeadsOrTails from '../src/get-heads-or-tails.js';
-const test = QUnit.test;
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+const { window } = new JSDOM(``);
+global.window = window;
+global.document = window.document;
 
-test('gets heads for less than .5', (assert) => {
-    // arrange
-    const number = .49;
-    const expected = 'heads';
-
-    // act
-    const result = getHeadsOrTails(number);
-
-    // assert
-    assert.equal(result, expected);
-});
-
-test('returns tails for equal or greater than .5', (assert) => {
-    const number = .5;
-    const expected = 'tails';
-
-    const result = getHeadsOrTails(number);
-
-    assert.equal(result, expected);
-});
+require = require('esm')(module);
+module.exports = require("./tests.js");
